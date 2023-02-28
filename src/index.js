88e3e1c806f88369d47b9f353036a55c3b65a391
paulo.almeida <paulo.almeida@mblabs.com.br>
2023-02-28 12:46:55 -0300

    deploy-v12

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './global.css'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
+import Home from './pages/Home'
 
 ReactDOM.render(
   <React.StrictMode>
